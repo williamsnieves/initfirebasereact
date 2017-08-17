c33995726adc22df47ed47092447637a8717f8c5
willians <williamsnieves@gmail.com>
2017-08-17 19:09:09 -0400

    added detail

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
+
 var config = {
-    apiKey: "AIzaSyD9F_yD3Ai-2KGaZUvcZ9YI3aldDBX3Ad8",
-    authDomain: "first-react-11733.firebaseapp.com",
-    databaseURL: "https://first-react-11733.firebaseio.com",
-    projectId: "first-react-11733",
-    storageBucket: "first-react-11733.appspot.com",
-    messagingSenderId: "45880380074"
-};
+ apiKey: "your data",
+ authDomain: "your data",
+ databaseURL: "your data",
+ projectId: "your data",
+ storageBucket: "your data",
+ messagingSenderId: "your data"
+ };
 firebase.initializeApp(config);
 
 export default firebase;
